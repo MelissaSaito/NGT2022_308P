6129486629b54ece56e8cb4d6ca9966147f05aa3
UchimuraYousuke <yousukeuchimura8113@gmail.com>
2022-02-28 00:06:32 +0900

    Player kyodou

--- NGT2022_308P/Assets/Script/PlayerContlroller.cs
+++ NGT2022_308P/Assets/Script/PlayerContlroller.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//ControllerA
+//ControllerB
+//ControllerX
+//ControllerY
+//ControllerZL
+//ControllerZR
+
 public class PlayerContlroller : MonoBehaviour
 {
     //speedMagnificationはスピード調節用
-    public float speedMagnification;
+    public float speedMagnification, slowSpeedMagnification;
 
     public Vector3 movingForce;
     private Vector3 movingDirection, movingVelocity;
     new Rigidbody rigidbody;
+
+    public GameObject UpperBody; 
 
     void Start()
     {
@@ -18,11 +28,62 @@ public class PlayerContlroller : MonoBehaviour
 
     private void Update()
     {
+        //コントローラーのLスティック&WSAD情報取得
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
+
+        // 0 or 1に変換
         movingDirection = new Vector3(x, 0, z);
-        movingDirection.Normalize();
-        movingVelocity = movingDirection * speedMagnification;
+        movingDirection.Normalize();// 0 or 1
+
+        //Idle時
+        if (movingDirection.x == 0 && movingDirection.z == 0)
+        {
+
+            Animator animator = GetComponent<Animator>();
+            animator.SetBool("Walk", false);
+            animator.SetBool("Idle", true);
+            movingVelocity.x = 0;
+            movingVelocity.z = 0;
+
+        }
+        else
+        {
+
+
+            //slow移動
+            if (Input.GetKey(KeyCode.E) || Input.GetButton("ControllerB"))
+            {
+                //確認用
+                Debug.Log("yeah");
+
+                //
+                Animator animator = GetComponent<Animator>();
+                animator.SetBool("Slow", true);
+
+                movingVelocity = movingDirection * slowSpeedMagnification;
+
+
+            }
+            //通常移動
+            else
+            {
+                Animator animator = GetComponent<Animator>();
+                animator.SetBool("Walk", true);
+                animator.SetBool("Slow", false);
+                movingVelocity = movingDirection * speedMagnification;
+
+            }
+
+
+        }
+
+        //聞き耳
+        if (Input.GetButton("ControllerZL") || Input.GetButton("ControllerZR"))
+        {
+            //上のCube無くす
+            UpperBody.SetActive(false);
+        }
     }
 
     void FixedUpdate()
@@ -30,3 +91,4 @@ public class PlayerContlroller : MonoBehaviour
         this.rigidbody.AddForce(movingVelocity, ForceMode.Force);
     }
 }
+
